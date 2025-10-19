adaa7b5d5a8bff33184ca78c9ccbfc7e959d2967
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-10-19 18:01:25 -0500

    feat(validation): agregar validaciones a RegisterRequest con Jakarta Validation
    - Se añadió @NotBlank, @Email y @Size para validar el correo electrónico.
    - Se agregó validación de tamaño y no vacío para la contraseña.
    - Se agregó validación para el nombre, asegurando no vacío y tamaño máximo.

--- src/main/java/com/practica/consultas/request/RegisterRequest.java
+++ src/main/java/com/practica/consultas/request/RegisterRequest.java
@@ -1,12 +1,21 @@
 package com.practica.consultas.request;
 
-/**
- *
- * @author Luis
- */
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record RegisterRequest(
+        @NotBlank(message = "El correo es obligatorio.")
+        @Email(message = "El formato del correo no es válido.")
+        @Size(max = 50, message = "El correo no puede tener más de 50 caracteres.")
         String correo,
+        
+        @NotBlank(message = "La contraseña es obligatoria.")
+        @Size(min = 6, max = 20, message = "La contraseña debe tener entre 6 y 20 caracteres.")
         String password,
+        
+        @NotBlank(message = "El nombre es obligatorio.")
+        @Size(max = 50, message = "El nombre no puede tener más de 50 caracteres.")
         String nombre) {
 
 }
